33a7fbdd9b19cd90f1ed1981e7dc854a4abd8c62
Matthew Boles <mboles@brightcove.com>
2023-01-24 09:28:45 -0500

    926

--- pull-all-v2.sh
+++ pull-all-v2.sh
@@ -1,10 +1,7 @@
+# Testing new pull all
 cd ..
 git submodule update --remote
 git submodule foreach git checkout main
 git submodule foreach git pull origin main 
 git commit -a -m "Commit in pull all"
-<<<<<<< HEAD
 git pull
-=======
-git pull
->>>>>>> 66d8a6e95cd3a0bd5028f0bc37a9f58ed8e6b708
