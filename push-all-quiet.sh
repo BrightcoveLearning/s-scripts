80d0718c6af9160c3ac2586c8bce1fd13a7127cf
Matthew Boles <mboles@brightcove.com>
2023-03-20 15:28:34 -0400

    check 325

--- push-all-quiet.sh
+++ push-all-quiet.sh
@@ -1,26 +1,29 @@
 echo "Enter your ${bold}COMMIT MESSAGE${normal}:"
 read COMMITMESSAGE 
 cd ..
+echo 'Pulling parent repo'
 git pull --quiet
+echo 'Building Jekyll'
 bundle update > /dev/null
 bundle exec jekyll build --trace --incremental > /dev/null
 setopt localoptions rmstarsilent
 rm -f _site/feed.xml
 bold=`tput bold`
 normal=`tput sgr0`
+echo 'Added changed files in parent repo'
 git pull --quiet
 git add . > /dev/null
-echo '13'
+echo 'Committing files in parent repo'
 git commit -m $COMMITMESSAGE --quiet
-echo '15'
+echo 'Pushing files in parent repo'
 git push --quiet
-echo '17'
+echo 'Getting all submodules'
 git submodule --quiet foreach "git fetch origin main --quiet"
-echo '19'
+echo 'Writing to submouldes'
 git submodule --quiet foreach "git checkout --quiet main  ; echo 'a' ; git pull --quiet; echo 'b' ;  git add . > /dev/null; echo 'c' ;  git commit -m '$COMMITMESSAGE' --quiet; echo 'd' ;  git push --quiet"
-echo '21'
+echo 'Syncing changed submodules with parent repo'
 git add . > /dev/null
 echo '23'
 git commit -m '$COMMITMESSAGE' --quiet
-echo '25'
+echo 'Pushing parent repo'
 git push --quiet
