b79facfde26c70c061eddde598b0f7fecaa5d6f2
Matthew Boles <mboles@brightcove.com>
2023-03-20 15:40:50 -0400

    check 338

--- push-all-quiet.sh
+++ push-all-quiet.sh
@@ -23,7 +23,7 @@ echo 'Writing to submouldes'
 git submodule --quiet foreach "git checkout --quiet main  ; echo 'a' ; git pull --quiet; echo 'b' ;  git add . > /dev/null; echo 'c' ;  git commit -m '$COMMITMESSAGE' --quiet; echo 'd' ;  git push --quiet"
 echo 'Syncing changed submodules with parent repo'
 git add . > /dev/null
-echo '23'
+echo 'Committing changes'
 git commit -m $COMMITMESSAGE --quiet
-echo 'Pushing parent repo'
+echo 'Pushing parent repo changes'
 git push --quiet
